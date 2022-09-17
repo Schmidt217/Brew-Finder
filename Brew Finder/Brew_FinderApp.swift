5eb0cdb535e4be19bcea638ed812ccc09a8fa86b
Mike Schmidt <schmidt217@gmail.com>
2022-09-17 11:20:23 -0600

    Initial Commit

---
+++ Brew Finder/Brew_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brew_FinderApp.swift
+//  Brew Finder
+//
+//  Created by Michael Schmidt on 9/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Brew_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
